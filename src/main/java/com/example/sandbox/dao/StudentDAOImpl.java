5f62821a187a8b29ca4d860bc55e5540b09299ac
Elvan Hidayat <elvanhd@gmail.com>
2023-05-22 23:12:59 +0700

    find by last name

--- src/main/java/com/example/sandbox/dao/StudentDAOImpl.java
+++ src/main/java/com/example/sandbox/dao/StudentDAOImpl.java
@@ -4,6 +4,7 @@ import com.example.sandbox.entity.Student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ public class StudentDAOImpl implements StudentDAO {
     private EntityManager entityManager;
 
     // inject entity manager using constructor injection
+    @Autowired
     public StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
@@ -37,6 +39,19 @@ public class StudentDAOImpl implements StudentDAO {
     public List<Student> findAll() {
         // create query
         TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameters
+        theQuery.setParameter("theData", theLastName);
 
         // return query results
         return theQuery.getResultList();
