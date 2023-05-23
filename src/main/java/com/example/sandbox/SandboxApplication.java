3811354b90ca40759de0868e0c2e3ee8d1f93c38
Elvan Hidayat <elvanhd@gmail.com>
2023-05-23 08:23:31 +0700

    deleting all students

--- src/main/java/com/example/sandbox/SandboxApplication.java
+++ src/main/java/com/example/sandbox/SandboxApplication.java
@@ -33,8 +33,17 @@ public class SandboxApplication {
 
             // updateStudent(studentDAO);
 
-            deleteStudent(studentDAO);
+            // deleteStudent(studentDAO);
+
+            deleteAllStudents(studentDAO);
         };
+    }
+
+    private void deleteAllStudents(StudentDAO studentDAO) {
+
+        System.out.println("Deleting all students");
+        int numRowsDeleted = studentDAO.deleteAll();
+        System.out.println("Deleted row count: " + numRowsDeleted);
     }
 
     private void deleteStudent(StudentDAO studentDAO) {
