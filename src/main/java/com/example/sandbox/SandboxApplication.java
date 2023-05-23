849af6c8cc9f0dc5f4e04d39ce173e2b3ccd379d
Elvan Hidayat <elvanhd@gmail.com>
2023-05-23 08:21:47 +0700

    deleting objects with jpa

--- src/main/java/com/example/sandbox/SandboxApplication.java
+++ src/main/java/com/example/sandbox/SandboxApplication.java
@@ -31,9 +31,17 @@ public class SandboxApplication {
 
             // queryForStudentsByLastName(studentDAO);
 
-            updateStudent(studentDAO);
+            // updateStudent(studentDAO);
 
+            deleteStudent(studentDAO);
         };
+    }
+
+    private void deleteStudent(StudentDAO studentDAO) {
+
+        int studentId = 3;
+        System.out.println("Deleting student id: " + studentId);
+        studentDAO.delete(studentId);
     }
 
     private void updateStudent(StudentDAO studentDAO) {
